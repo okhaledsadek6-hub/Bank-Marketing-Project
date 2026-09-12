fc2746c227e6822e87839b1e91c03df77829088f
okhaledsadek6-hub <o.khaled.sadek6@gmail.com>
2026-09-12 19:47:33 +0300

    Update app.py

--- Final Project/app.py
+++ Final Project/app.py
@@ -250,7 +250,7 @@ hr {
 # LOAD FILES
 # =========================================================
 
-BASE_DIR = os.path.dirname(os.path.abspath(_file_))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @st.cache_resource
 def load_model():
