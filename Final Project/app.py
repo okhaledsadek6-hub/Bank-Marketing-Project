75d5f50a9c335d77af950a12762501831184ca51
okhaledsadek6-hub <o.khaled.sadek6@gmail.com>
2026-09-14 22:31:47 +0300

    Update app.py

--- Final Project/app.py
+++ Final Project/app.py
@@ -385,8 +385,7 @@ with st.sidebar:
         ],
         label_visibility="collapsed"
     )
-         """
-        <div style="
+        st.markdown("""<div style="<div style="
             text-align:center;
             color:#94a3b8;
             margin-bottom:25px;
